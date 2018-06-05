f632b91de37471af16421749446127437fcad741
David Yi <davidyi@Davids-MBP-2.localdomain>
2018-06-05 16:08:05 -0700

    added google map to result page

--- src/components/GroupsTable.js
+++ src/components/GroupsTable.js
@@ -1,24 +1,14 @@
 import React, { Component } from 'react';
 import { Button, ButtonGroup } from 'react-bootstrap'
-import WithAuth from '../api/WithAuth'
-import {	 get_user_groups,add_user_group,delete_user_group,update_user_group} from '../api/GroupAPI'
-import { get_user } from '../api/UserAPI'
 
 class GroupsTable extends Component {
-  componentWillMount() {
-     console.log("Inside of GroupsTable")
-     console.log("User ID" + this.props.userId)
-     let groups = get_user_groups(this.props.userId)
-
-  }
 
   render() {
     return (
       <div>
         <h1><strong>Groups</strong></h1><hr/>
-        <p>User ID is: {this.props.userId}</p>
-        <ul className="list-group">
-          <li className="list-group-item">
+        <ul class="list-group">
+          <li class="list-group-item">
             <span>Family</span>
             <ButtonGroup className="pull-right" bsSize="small">
               <Button bsStyle="success">Play</Button>
@@ -26,7 +16,7 @@ class GroupsTable extends Component {
               <Button bsStyle="danger">Delete</Button>
             </ButtonGroup>
           </li>
-          <li className="list-group-item">
+          <li class="list-group-item">
             <span>Work Friends</span>
             <ButtonGroup className="pull-right" bsSize="small">
               <Button bsStyle="success">Play</Button>
@@ -34,22 +24,22 @@ class GroupsTable extends Component {
               <Button bsStyle="danger">Delete</Button>
             </ButtonGroup>
           </li>
-          <li className="list-group-item"><span>Poker Buds</span>
+          <li class="list-group-item"><span>Poker Buds</span>
             <ButtonGroup className="pull-right" bsSize="small">
               <Button bsStyle="success">Play</Button>
               <Button bsStyle="primary">Edit</Button>
               <Button bsStyle="danger">Delete</Button>
             </ButtonGroup>
           </li>
-          <li className="list-group-item"><span>Golf Pals</span>
+          <li class="list-group-item"><span>Golf Pals</span>
             <ButtonGroup className="pull-right" bsSize="small">
               <Button href="../Login" bsStyle="success">Play</Button>
               <Button bsStyle="primary">Edit</Button>
               <Button bsStyle="danger">Delete</Button>
             </ButtonGroup>
           </li>
-          <li className="list-group-item green"><span>Add New</span>
-            <Button id="addGroup" className="pull-right green"><i className="fas fa-plus-square"></i></Button>
+          <li class="list-group-item green"><span>Add New</span>
+            <Button id="addGroup" className="pull-right green"><i class="fas fa-plus-square"></i></Button>
           </li>
         </ul>
       </div>
@@ -60,7 +50,7 @@ class GroupsTable extends Component {
 
 
 }
-export default WithAuth(GroupsTable);
+export default GroupsTable;
 
 
 
