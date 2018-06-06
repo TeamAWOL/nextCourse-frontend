b30fe1facf333b3847eafa460c52998eb64602fd
dbrow437 <dbrow437@gmail.com>
2018-06-06 14:44:18 -0700

    about page complete

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,20 +1,39 @@
 import React, { Component } from 'react';
 import '../css/About.css';
-import { Jumbotron, Button } from 'react-bootstrap';
+import { Jumbotron } from 'react-bootstrap';
 
 
 class About extends Component {
 
   render() {
     return (
+      <div className='about'>
         <div className='jumbo'>
-        <Jumbotron>
-          <h1>The Feed Me Story</h1>
+          <Jumbotron>
+            <h1>The Feed Me Story</h1>
+            <p>
+              Four Friends Wanted to Make Restaurant Selections Easier
+            </p>
+        </Jumbotron>
+        </div>
+
+        <div className='about-body'>
           <p>
-            Four Friends Wanted to Make Restaurant Selections Easier
+            <h3> Welcome fellow foodie -- we are glad you decided to check us out!</h3>
           </p>
-      </Jumbotron>
-      </div>
+            <h4> One day, like many days, we could not choose what to eat for lunch. Damon, wanted sushi, Steven wanted pizza, Armando wanted a burger and David wanted fish tacos.  We spent the first ten minutes of our lunch-break arguing on a selection!  Finally, we decided pizza but we all thought there had to be a better way </h4>
+
+        </div>
+
+        <div className='mv-statement'>
+          <p>
+            <h3>Our Mission:</h3><h4> Create a fun restaurant selector that supplies the restaurant and the location allowing you more time to enjoy the food</h4>
+          </p>
+          <p>
+             <h3>Our Values:</h3> <h4>Fair and social -- everyone has the same odds to win with our algorithm</h4>
+          </p>
+        </div>
+    </div>
     );
   }
 }
