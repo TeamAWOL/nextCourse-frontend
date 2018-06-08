7c305353f543a1c98e20e04496aee9f18d2c12d3
A-Nunez <nunez.armando96@gmail.com>
2018-06-08 13:56:35 -0700

    All workin

---
+++ src/components/Modal.js
@@ -0,0 +1,69 @@
+import React, { Component } from 'react';
+import '../css/Modal.css';
+import { Modal, ButtonToolbar, Button } from 'react-bootstrap';
+import { Redirect } from 'react-router-dom'
+
+
+
+class GameModal extends React.Component {
+  constructor(props, context) {
+    super(props, context);
+
+    this.handleShow = this.handleShow.bind(this);
+    this.handleHide = this.handleHide.bind(this);
+
+    this.state = {
+      show: false,
+      switch: false
+    };
+  }
+
+  handleShow() {
+    this.setState({ show: true });
+  }
+
+  handleHide() {
+    this.setState({ switch: true });
+  }
+
+  render() {
+    return (
+      <ButtonToolbar>
+        <Button bsStyle="primary" onClick={this.handleShow}>
+        Play Game
+        </Button>
+
+        <Modal
+          {...this.props}
+          show={this.state.show}
+          onHide={this.handleHide}
+          dialogClassName="custom-modal"
+        >
+          <Modal.Header closeButton>
+            <Modal.Title id="ed-modal-title-lg">
+              Selecting Restaurant
+            </Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <p>
+            <div className="modalContainer">
+                <div className="item item-1"></div>
+                <div className="item item-2"></div>
+                <div className="item item-3"></div>
+                <div className="item item-4"></div>
+            </div>
+            </p>
+          </Modal.Body>
+          <Modal.Footer>
+            <Button onClick={this.handleHide}>Click for Results</Button>
+          </Modal.Footer>
+          {this.state.switch && <Redirect to={'/Result'}/> }
+        </Modal>
+      </ButtonToolbar>
+    );
+  }
+}
+
+
+
+export default GameModal;
