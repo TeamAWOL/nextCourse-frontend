ee47c4c26581495850713d06bfa784c2f8179c6d
A-Nunez <nunez.armando96@gmail.com>
2018-06-08 13:51:36 -0700

    fixed header Save Point

--- src/pages/Game.js
+++ src/pages/Game.js
@@ -1,17 +1,25 @@
 import React, { Component } from 'react';
 import '../css/Game.css';
 import AuthService from '../api/AuthService';
+import GameModal from '../components/Modal.js'
+
 
 class Game extends Component {
   componentWillMount() {
-     
+
   }
 
   render() {
     return (
       <div id="game-page">
         <h1>GaMe PaGe</h1>
-      </div>
+
+        <div>
+          <GameModal />
+        </div>
+
+  </div>
+
     );
   }
 
