f7757872f64c4b43ec858cb90ef051e82c99bba9
A-Nunez <nunez.armando96@gmail.com>
2018-06-08 20:04:30 -0700

    working play and delete buttons

--- src/components/GroupsTable.js
+++ src/components/GroupsTable.js
@@ -1,17 +1,24 @@
 import React, { Component } from 'react';
-import { Button, ButtonGroup, Grid, Col, Row, ListGroup, ListGroupItem } from 'react-bootstrap'
+import '../css/Modal.css';
 import WithAuth from '../api/WithAuth'
+import { Button, ButtonGroup, Grid, Col, Row, ListGroup, ListGroupItem, Modal } from 'react-bootstrap'
 import { get_user_groups, delete_user_group } from '../api/GroupAPI'
-import { Link } from 'react-router-dom'
-import GameModal from '../components/Modal.js'
+import { Link, Redirect } from 'react-router-dom'
+
+
 
 
 class GroupsTable extends Component {
   constructor(props){
     super(props)
+    this.handleShow = this.handleShow.bind(this);
+    this.handleHide = this.handleHide.bind(this);
+
     this.state = {
-      groups: []
-    }
+      groups: [],
+      show: false,
+      redirect: false
+    };
   }
 
   componentWillMount() {
@@ -29,6 +36,14 @@ class GroupsTable extends Component {
     console.log("yoooo");
   }
 
+  handleShow() {
+    this.setState({ show: true });
+  }
+
+  handleHide() {
+    this.setState({ redirect: true });
+  }
+
   render() {
     return (
       <div>
@@ -43,15 +58,39 @@ class GroupsTable extends Component {
                 <h4>{group.name}</h4>
                 <ButtonGroup className="pull-right">
                 <Button href="../Feed" name={group.id} onClick={this.handleDeleteClick.bind(this)} bsSize="small" bsStyle="danger">Delete</Button>
-                <Link to={`/EditGroup/${group.name}`}><Button bsSize="small" bsStyle="primary">Edit</Button></Link>
-                <Button bsSize="small" bsStyle="success">Play</Button>
-                <div><GameModal /></div>
-
+                <Link to={`/EditGroup/${group.name}`}><Button className="pull-left" bsSize="small" bsStyle="primary">Edit</Button></Link>
+                <Button id="play_text" bsSize="small" bsStyle="success" onClick={this.handleShow}>Play</Button>
                 </ButtonGroup>
               </ListGroupItem>
             )
           })}
         </ListGroup>
+        <Modal
+          {...this.props}
+          show={this.state.show}
+          onHide={this.handleHide}
+          dialogClassName="custom-modal"
+        >
+          <Modal.Header closeButton>
+            <Modal.Title id="contained-modal-title-lg">
+              Selecting Restaurant
+            </Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <p>
+            <div className="modalContainer">
+                <div className="item item-1"></div>
+                <div className="item item-2"></div>
+                <div className="item item-3"></div>
+                <div className="item item-4"></div>
+            </div>
+            </p>
+          </Modal.Body>
+          <Modal.Footer>
+            <Button onClick={this.handleHide}>Click for Results</Button>
+          </Modal.Footer>
+          {this.state.redirect && <Redirect to={'/Result'}/> }
+        </Modal>
       </div>
     )
   }
